9da003a3f0ac6b8a76c7de800f0b04c4129d2371
Mahesh Kumar G <maheshh.kumar1508@gmail.com>
2025-05-30 00:42:10 +0530

    authentication - user

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,16 +1,20 @@
 import { Router } from "express";
+import { getUser, getUsers } from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
-userRouter.get('/', (req, res) => res.send({ message: 'GET - retrieve all users' }));
+// userRouter.get('/', (req, res) => res.send({ message: 'GET - retrieve all users' }));
 
-userRouter.get('/:id', (req, res) => res.send({ message: 'GET - retrieve user by id' }));
+// userRouter.get('/:id', (req, res) => res.send({ message: 'GET - retrieve user by id' }));
 
-userRouter.post('/', (req, res) => res.send({ message: 'POST - create a user' }));
+// userRouter.post('/', (req, res) => res.send({ message: 'POST - create a user' }));
 
-userRouter.put('/:id', (req, res) => res.send({ message: 'PUT - update a user by id' }));
+// userRouter.put('/:id', (req, res) => res.send({ message: 'PUT - update a user by id' }));
 
-userRouter.delete('/:id', (req, res) => res.send({ message: 'DELETE - a user by id' }));
+// userRouter.delete('/:id', (req, res) => res.send({ message: 'DELETE - a user by id' }));
 
+userRouter.get('/', getUsers);
+
+userRouter.get('/:id', getUser);
 
 export default userRouter;
