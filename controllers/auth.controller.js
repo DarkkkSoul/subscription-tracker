81436d7a17cb07ebea23abaef5a99c1779e78dc5
Mahesh Kumar G <maheshh.kumar1508@gmail.com>
2025-05-29 20:10:19 +0530

    authentication - signIn

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -52,6 +52,46 @@ export const signUp = async (req, res, next) => {
    }
 };
 
-export const signIn = async (req, res, next) => { }
+export const signIn = async (req, res, next) => {
+   try {
+      const { email, password } = req.body;
+
+      const user = await User.findOne({ email });
+
+      // check is user not found
+      if (!user) {
+         const error = new Error("User Not Found");
+         error.statusCode = 404;
+         throw error;
+      }
+
+      // if found then
+      // 1. compare password
+      const isPassValid = await bcrypt.compare(password, user.password);
+
+      // 2. check if password is wrong
+      if (isPassValid) {
+         const error = new Error('Incorrect Password');
+         error.statusCode = 401;
+         throw error;
+      }
+
+      // 3. create token if password is correct
+      const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: JWT_EXPIRY });
+
+      // 4. return json and status code
+      res.status(200).json({
+         success: true,
+         message: 'User Found - Signed in Successfully',
+         data: {
+            token,
+            user,
+         }
+      })
+
+   } catch (error) {
+      next(error);
+   }
+}
 
 export const signOut = async (req, res, next) => { }
